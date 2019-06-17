e1a5a16201153d8609cf2aa29f2838cd6273112c
Admin <dominik.tulak@email.cz>
2019-06-17 20:50:09 +0200

    added Programs and Users

---
+++ MobileApp/MobileApp/Model/Programs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileApp.Model
+{
+    class Programs
+    {
+        public static string install(string name, bool sudo)
+        {
+            string outp = "apt-get install ";
+            outp += name;
+            if (sudo) { outp = "sudo " + outp; }
+            return outp;
+        }
+        public static string remove(string name, bool sudo)
+        {
+            string outp = "apt-get remove ";
+            outp += name;
+            if (sudo) { outp = "sudo " + outp; }
+            return outp;
+        }
+        public static string purge(string name, bool sudo)
+        {
+            string outp = "apt-get purge ";
+            outp += name;
+            if (sudo) { outp = "sudo " + outp; }
+            return outp;
+        }
+        public static string autoremove(bool sudo)
+        {
+            string outp = "apt-get auto-remove ";
+            if (sudo) { outp = "sudo " + outp; }
+            return outp;
+        }
+    }
+}
